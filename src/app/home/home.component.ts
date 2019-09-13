eaff24fb00483e32052e1452646a3d4ad5acbc7a
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-13 08:24:17 +0530

    cutom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +14,31 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.firstObsSubcription = interval(1000).subscribe(
-    (count) => {
-      console.log({count});
+    // this.firstObsSubcription = interval(1000).subscribe(
+    // (count) => {
+    //   console.log({count});
+    // });
+    const cutomIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('count is greater than 3'));
+
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubcription = cutomIntervalObservable.subscribe((data) => {
+      console.log({ data });
+    }, (error) => {
+      alert(error.message);
+    }, () => {
+      console.log('completed!!');
     });
   }
   ngOnDestroy(): void {
